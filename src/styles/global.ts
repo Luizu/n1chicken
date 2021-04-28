1fe6f95756f961e425ba66b066efb859dd37ad52
luizu <luizucastilho@gmail.com>
2021-04-27 21:39:19 -0300

    Initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720PX) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  body, input, button {
+    font: 14px Roboto, sans-serif;
+  }
+
+
+  button {
+    cursor: pointer;
+  }
+`;
