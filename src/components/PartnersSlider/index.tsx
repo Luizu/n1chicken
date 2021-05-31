9834a714819deeeeb7f3f0299d52027f3cede59b
Luizu <luizucastilho@gmail.com>
2021-05-31 16:28:25 -0300

    Chore: Add responsivity to partners slider

--- src/components/PartnersSlider/index.tsx
+++ src/components/PartnersSlider/index.tsx
@@ -3,15 +3,18 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 import { Container, ImageContainer, Image } from './styles';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
 export function PartnersSlider(): JSX.Element {
+  const { width } = useWindowDimensions();
+
   const settings = {
     dots: true,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 3000,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: width <= 600 ? 1 : width <= 1000 ? 3 : 5,
     slidesToScroll: 1,
   };
 
